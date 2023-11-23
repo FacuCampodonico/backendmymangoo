e3c8b450a7d80a7c85cc19f6aa2c4d8ea6d28d64
facundo campodonico <facundocampodonico04@gmail.com>
2023-11-23 12:41:19 -0300

    cambie un nombre nomas

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const taskRoutes = require('./routes/routes');
+const userRoutes = require('./routes/routes');
 
 const app = express();
 
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use(taskRoutes)
+app.use(userRoutes)
 
 app.listen(3000)
 console.log('server on port 3000')
