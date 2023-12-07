f22168a9738d4330b1000f2c2e73986ffd841339
FacuCampodonico <96396524+FacuCampodonico@users.noreply.github.com>
2023-12-07 10:41:19 -0300

    Update db.sql para la columna name

--- database/db.sql
+++ database/db.sql
@@ -3,5 +3,6 @@ CREATE DATABASE usersdb
 CREATE TABLE users(
     id  SERIAL PRIMARY KEY,
     mail VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 )
