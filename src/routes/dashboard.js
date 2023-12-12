3fe2eb6743bca5b98e327d2bb0cbe84b365a8934
Facundo Campodonico <facundocampodonico04@gmail.com>
2023-12-12 12:32:13 -0300

    ahora devuelve el nombre una vez que se logea

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -5,7 +5,7 @@ const authorization = require("../middleware/authorization")
 router.get("/", authorization, async (req, res) => {
     try {
 
-        const user = await pool.query('SELECT mail FROM users WHERE id = $1',[
+        const user = await pool.query('SELECT name FROM users WHERE id = $1',[
             req.user
         ])
 
